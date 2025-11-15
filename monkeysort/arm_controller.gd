861ea20a91e61973a19ebf604c3731035581e3e6
Jesse Rheal <jessecolerheal@live.com>
2025-11-14 21:18:32 -0600

    Replaced KinematicBody2D with Area2D for hand

--- monkeysort/arm_controller.gd
+++ monkeysort/arm_controller.gd
@@ -3,7 +3,8 @@ extends Node2D
 @export var color: Color
 
 func _physics_process(delta):
+	%HandArea.global_position = get_global_mouse_position()
 	queue_redraw()
 
 func _draw():
-	draw_line(global_position, -global_position + get_global_mouse_position(), color, 40)
+	draw_line(Vector2(0, 0), %HandArea.global_position, color, 40)
