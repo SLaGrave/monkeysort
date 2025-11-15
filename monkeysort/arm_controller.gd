1407751fde250ff6c848e4e70c14ff840e6019d1
Jesse Rheal <jessecolerheal@live.com>
2025-11-14 21:04:05 -0600

    Fixed the script

--- monkeysort/arm_controller.gd
+++ monkeysort/arm_controller.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
-@onready
-var color_rect: ColorRect = $ColorRect
+@export var color: Color
 
 func _physics_process(delta):
-	position = get_viewport().get_mouse_position()
 	queue_redraw()
 
 func _draw():
-	draw_line(Vector2(0, 0), Vector2(640, 750) - position, color_rect.color, 40)
+	draw_line(global_position, -global_position + get_global_mouse_position(), color, 40)
