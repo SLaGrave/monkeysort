0a1781f7bd0c83c8c34c68ae904207f938edda08
Sam LaGrave <srlagrave@gmail.com>
2025-11-16 14:35:34 -0600

    Simplify.

--- monkeysort/banan.gd
+++ monkeysort/banan.gd
@@ -1,18 +1,13 @@
 class_name Banan extends CharacterBody2D
 
-var conveyor_speed: float = 100.0
-
 @export var on_conveyor: bool = true
-var speed := 100.0
+
+# Initial (on conveyor) settings
+var speed := GlobalSettings.conveyor_speed
 var direction := Vector2.LEFT
-var gravity := 100.0
 
 func _physics_process(delta: float) -> void:
-	if on_conveyor:
-		speed = 100.0
-		direction = Vector2.LEFT
-	else:
-		speed += delta * gravity
+	if not on_conveyor:
+		speed += delta * GlobalSettings.gravity
 		direction = Vector2.DOWN
-		
 	move_and_collide(delta * speed * direction)
---
+++ monkeysort/global_settings.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@export var conveyor_speed: float = 100.0
+@export var gravity: float = 100.0
