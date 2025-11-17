05a8ad7b12a3cc1a049dee0356edb493ff808361
Jesse Rheal <jessecolerheal@live.com>
2025-11-16 19:44:29 -0600

    Swapped to RigidBody2D for bananas

--- monkeysort/scripts/arm_controller.gd
+++ monkeysort/scripts/arm_controller.gd
@@ -7,6 +7,7 @@ func _physics_process(_delta) -> void:
 	%HandArea.global_position = get_global_mouse_position()
 	if grabbed_item != null:
 		if Input.is_action_just_released("grab"):
+			grabbed_item.state = Banan.StateMachine.FALLING
 			grabbed_item = null
 			return
 		grabbed_item.global_position = %HandArea.global_position
@@ -23,11 +24,4 @@ func _input(event: InputEvent) -> void:
 		if not bodies.is_empty():
 			if bodies[0] is Banan:
 				grabbed_item = bodies[0]
-				bodies[0].on_conveyor = false
-
-
-func _on_hand_area_body_entered(_param) -> void:
-	# The HandArea emits a signal to a slot with this name (that didn't exist).
-	# Probably a carryover from an old idea of Jesse's, but I didn't want to
-	# unbind it if it was needed, so I made this stub.
-	pass
+				grabbed_item.state = Banan.StateMachine.GRABBED
--- monkeysort/scripts/banan.gd
+++ monkeysort/scripts/banan.gd
@@ -1,19 +1,34 @@
-class_name Banan extends CharacterBody2D
+class_name Banan extends RigidBody2D
 
 enum BananaType { RIPE, UNRIPE, ROTTEN }
+enum StateMachine { ON_CONVEYOR, GRABBED, FALLING }
 
 @export var banana_type: BananaType
 
-@export var on_conveyor: bool = true
+var state := StateMachine.ON_CONVEYOR
 
 # Initial (on conveyor) settings
 var speed := Globals.conveyor_speed
 var direction := Vector2.LEFT
 
 func _physics_process(delta: float) -> void:
-	if not on_conveyor:
-		speed += delta * Globals.gravity
-		direction = Vector2.DOWN
+	match state:
+		StateMachine.ON_CONVEYOR:
+			freeze = false
+			gravity_scale = 0.0
+			speed = 100.0
+			direction = Vector2.LEFT
+		StateMachine.GRABBED:
+			collision_layer = 0
+			collision_mask = 0
+			freeze = true
+		StateMachine.FALLING:
+			gravity_scale = 1.0
+			collision_layer = 1
+			collision_mask = 1
+			freeze = false
+			direction = Vector2.DOWN
+		
 	var collision_info = move_and_collide(delta * speed * direction)
 	if collision_info:
 		var collider = collision_info.get_collider()
