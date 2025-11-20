c12de313b97c4d1419f59c483228e84c48c4f3ba
Sam LaGrave <srlagrave@gmail.com>
2025-11-19 19:44:26 -0600

    Winning a level looks better

--- monkeysort/scripts/game.gd
+++ monkeysort/scripts/game.gd
@@ -7,6 +7,8 @@ class_name Game extends Node2D
 @onready var progress_bar = %ProgressBar
 @onready var game_over_audio = $"Sound Effects (non-Box & non-Banan))/GameOver"
 
+var sams_flag: bool = false
+
 func _ready() -> void:
 	Globals.mistake_made.connect(_on_mistake_made)
 	Globals.score_increased.connect(_on_score_increased)
@@ -16,6 +18,7 @@ func _process(delta: float) -> void:
 	#print(Globals.time_elapsed)
 	progress_bar.value = Globals.time_elapsed
 	if progress_bar.value >= progress_bar.max_value:
+	#if progress_bar.value >= 10:
 		win_level()
 
 func _input(event):
@@ -44,12 +47,26 @@ func restart_current_level():
 	var timer := Timer.new()
 	timer.one_shot = true
 	timer.wait_time = 2.0
-	timer.timeout.connect(_second_half)
+	timer.timeout.connect(_restart_current_second_half)
 	add_child(timer)
 	timer.start()
 	
-func _second_half():
+func _restart_current_second_half():
 	set_level(level)
 
 func win_level():
+	if sams_flag:
+		return
+	sams_flag = true
+	gui.add_bad_text("You won this level!\nOn to the next!")
+	# Delay the restart by 2 seconds
+	var timer := Timer.new()
+	timer.one_shot = true
+	timer.wait_time = 2.0
+	timer.timeout.connect(_win_level_second_half)
+	add_child(timer)
+	timer.start()
+
+func _win_level_second_half():
+	sams_flag = false
 	set_level(level+1)
