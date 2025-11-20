3a86b84d2255b42539e9d332422eca256d5ebc7d
Sam LaGrave <srlagrave@gmail.com>
2025-11-19 19:37:17 -0600

    3 boxes 3 banans 3 levels

--- monkeysort/scripts/box.gd
+++ monkeysort/scripts/box.gd
@@ -2,11 +2,30 @@ class_name BananBox extends Area2D
 
 @export var accepts: Banan.BananaType
 
+@onready var main_game = $".."
+@onready var collision_shape_2d = $CollisionShape2D
+
 @onready var audio_correct: AudioStreamPlayer2D  = $AudioCorrect
 @onready var audio_incorrect: AudioStreamPlayer2D = $AudioIncorrect
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	Globals.game_reset.connect(set_visibility)
+	set_visibility()
+
+func set_visibility():
+	collision_shape_2d.disabled = false
+	visible = true
+	match accepts:
+		Banan.BananaType.ROTTEN:
+			if main_game.level < 1:
+				collision_shape_2d.disabled = true
+				visible = false
+		Banan.BananaType.UNRIPE:
+			if main_game.level < 2:
+				collision_shape_2d.disabled = true
+				visible = false
+	queue_redraw()
 
 func _on_body_entered(body):
 	if body is Banan:
--- monkeysort/scripts/game.gd
+++ monkeysort/scripts/game.gd
@@ -15,14 +15,16 @@ func _process(delta: float) -> void:
 	Globals.time_elapsed += delta
 	#print(Globals.time_elapsed)
 	progress_bar.value = Globals.time_elapsed
+	if progress_bar.value >= progress_bar.max_value:
+		win_level()
 
 func _input(event):
 	if event.is_action_pressed("close"):
 		get_tree().quit()
 
 func set_level(val: int):
-	Globals.reset()
 	level = val
+	Globals.reset()
 	get_tree().call_group("banans", "queue_free")
 
 func _on_score_increased():
@@ -48,3 +50,6 @@ func restart_current_level():
 	
 func _second_half():
 	set_level(level)
+
+func win_level():
+	set_level(level+1)
