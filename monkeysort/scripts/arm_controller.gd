4762766121ab48f4a04a060c4c3f6491b54a65b9
Jesse Rheal <jessecolerheal@live.com>
2025-11-20 21:46:53 -0600

    Wiggly

--- monkeysort/scripts/arm_controller.gd
+++ monkeysort/scripts/arm_controller.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
-@export var color: Color
+@export var paw_color: Color
+@export var arm_color: Color
 var grabbed_item: Banan
 
+var desired_power := 0.0
+var power := 0.0
+var last_mouse_position := Vector2()
 func _physics_process(_delta) -> void:
 	%HandArea.global_position = get_global_mouse_position()
 	if grabbed_item != null:
@@ -11,12 +15,26 @@ func _physics_process(_delta) -> void:
 			grabbed_item = null
 			return
 		#grabbed_item.global_position = %HandArea.global_position
+	desired_power = -(get_global_mouse_position() - last_mouse_position)[0] * 4.0
+	power = lerp(power, desired_power, 0.3)
 	queue_redraw()
-
+	last_mouse_position = get_global_mouse_position()
 
 func _draw() -> void:
-	draw_line(Vector2(0, 0), %HandArea.position, color, 40)
-
+	var midway := Vector2(0, 0).lerp(%HandArea.position, 0.5)
+	var cross := midway.rotated(PI / 2.0).normalized()
+	var points: PackedVector2Array = []
+	points.resize(100)
+	var yoinky = func(x): return cross * (-1.0 / 2500.0 * (x - 50)**2 + 1)
+	var sploinky = func(x): return cross * 10.0 * sin(x / 10.0 + Globals.time_elapsed)
+	for i in range(100):
+		var if_it_were_straight: Vector2 = lerp(Vector2(0, 0), %HandArea.position, i / 100.0)
+		var end: Vector2 = if_it_were_straight + power * yoinky.call(i) + sploinky.call(i)
+		points[i] = end
+	
+	draw_polyline(points.slice(0, 91), arm_color, 40, true)
+	draw_circle(points[99], 20.0, paw_color, true)
+	draw_polyline(points.slice(90, 100), paw_color, 40, true)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("grab"):
